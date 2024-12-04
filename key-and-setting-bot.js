e4c0c594834d7f12b4a209b208bd429c6e09a516
Rerezz Official <thegamergemu0001@gmail.com>
2024-12-04 19:21:58 +0700

    Create key-and-setting-bot.js

---
+++ key-and-setting-bot.js
@@ -0,0 +1,5 @@
+global.username = "Rerez"
+global.password = "0208"
+global.konek = "6285216955233"
+global.salurann = "https://whatsapp.com/channel/0029Vat4lwpKLaHoS0MAwj3Z"
+global.gcbottt = "https://chat.whatsapp.com/LYVP0yvVzzY4qx2rLvZR6E"
